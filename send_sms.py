637906fc632e2d2ddec0ae2826dc56883c7f2b50
katemwalker <kmwalker@utexas.edu>
2019-07-22 14:35:39 -0500

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC96b8f85325af280e038622314a88629a'
-auth_token = 'e0514dfbc1dada56a857b8ad859405df'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 message = client.messages \

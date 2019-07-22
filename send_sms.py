73cefb90193244b642fd8869ef68ac20cab9db7f
katemwalker <kmwalker@utexas.edu>
2019-07-22 14:35:56 -0500

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -11,8 +11,8 @@ client = Client(account_sid, auth_token)
 message = client.messages \
                 .create(
                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+18329091548',
-                     to='+12818060701'
+                     from_='+',
+                     to='+'
                  )
 
 print(message.sid)
